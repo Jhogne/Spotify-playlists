b074f2a0f4a5f4584e360e432e5b6126b00bf732
JonazIT <hogne@student.chalmers.se>
2020-01-27 14:44:40 +0100

    Better structure and allows multiple artists at the same time

--- main.py
+++ main.py
@@ -4,38 +4,76 @@ import sys
 import spotipy
 import spotipy.util as util
 
-if len(sys.argv) > 2:
+def authorize(username):
+    scope = 'playlist-modify-private playlist-modify-public playlist-read-private'
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+    else:
+        print("Can't get token for", username)
+    return sp    
+
+def get_all_results(results):
+    items = results['items']
+    while results['next']:
+        results = sp.next(results)
+        items.extend(results['items'])
+    return items
+
+def add_to_playlist(username, playlist_id, tracks):
+    chunks = [tracks[x:x+100] for x in range(0,len(tracks), 100)]
+    for i in chunks:
+        sp.user_playlist_add_tracks(username, playlist_id, i, position=None)
+
+def get_fields(field, l):
+    return [i[field] for i in l]
+
+def get_playlist_id(playlist_name):
+    playlists = sp.user_playlists(username)
+    names = get_fields('name', playlists['items'])
+    playlist_id = ''
+    for i in playlists['items']:
+        if i['name'] == playlist_name:
+            playlist_id = i['id']
+            break
+
+    if playlist_id == '':
+        playlist_id = sp.user_playlist_create(username, playlist_name, public=True, description='')['id']
+    
+    return playlist_id
+
+
+
+
+
+
+if len(sys.argv) > 3:
     username = sys.argv[1]
-    artist_name = sys.argv[2]
+    playlist_name = sys.argv[2]
+    artist_names = sys.argv[3:len(sys.argv)]
 else:
     print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
     sys.exit()
 
-scope = 'playlist-modify-private, playlist-modify-public, user-library-read'
-token = util.prompt_for_user_token(username, scope)
+sp = authorize(username)
+playlist_id = get_playlist_id(playlist_name)
 
-playlist = 'spotify:user:jonas.hogne:playlist:6D1u6A4DmGiKceXhGlJyDM'
-track = ['spotify:track:6rqhFgbbKwnb9MLmUQDhG6']
+# Gets the first aritst id when searching for artists with the given prompt
+artist_ids = []
+for name in artist_names:
+    artist_ids.append(sp.search(q=name, type='artist')['artists']['items'][0]['id'])
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    sp.trace = False
-    results = sp.user_playlist_add_tracks(username, playlist, track, position=None)
-    print(results)
-else:
-    print("Can't get token for", username)
+albums = []
+for artist_id in artist_ids:
+    albums += get_all_results(sp.artist_albums(artist_id, album_type='album,single'))
 
-artist_id = sp.search(q=artist_name, type='artist')['artists']['items'][0]['id']
-
-results = sp.artist_albums(artist_id, album_type='album,single')
-albums = results['items']
-while results['next']:
-    results = sp.next(results)
-    albums.extend(results['items'])
-
+tracks = []
 for album in albums:
-    print(album['name'])
-    tracks = sp.album_tracks(album['id'])['items']
-    for i in tracks:
-        print(i['name'])
-    print()
+    album_tracks = sp.album_tracks(album['id'])['items']
+    tracks += get_fields('id', album_tracks)
+
+playlist_tracks = get_all_results(sp.user_playlist_tracks(username,playlist_id))
+orig_tracks = [x['track']['id'] for x in playlist_tracks]
+add_to_playlist(username, playlist_id, list(set(tracks) - set(orig_tracks)))
